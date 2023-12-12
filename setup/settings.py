02f7ab3549859c114956b5ea11ae6e3ed0f06dc3
Leandro22061988 <89951794+Leandro22061988@users.noreply.github.com>
2023-12-11 23:14:27 -0300

    correção arquivos staticos

--- setup/settings.py
+++ setup/settings.py
@@ -52,7 +52,8 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'base')],
+        'DIRS': [ BASE_DIR / 'base'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,8 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'base/global/static'),
-    os.path.join(BASE_DIR, 'contato/static'),            
+
+    os.path.join(BASE_DIR, 'base/static'),
+                
 ]
 AUTH_USER_MODEL = 'usuarios.CustomUser'
 LOGIN_URL = 'login'  
